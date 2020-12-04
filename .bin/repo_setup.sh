ed3a6eeba89ed3840cbe9fdad312f0af85c9cfb9
Jon Newton <newt@hey.com>
2020-12-04 15:23:04 -0500

    a script to setup my git repos

---
+++ .bin/repo_setup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+usage() 
+{ 
+  echo "Usage: repo_setup.sh [-f] <template> "
+  echo "   -f  force overwrite existing files"
+}
+
+if [ $# -eq 1 ]; then
+  templateDir="$1"
+  cpArgs="-i"
+elif [ $# -eq 2 ]; then
+  if [ "$1" = "-f" ]; then
+    cpArgs="-f"
+  else
+    usage
+    exit 3
+  fi
+  templateDir="$2"
+else
+  usage
+  exit 1
+fi
+
+if [ ! -e $templateDir/.git ]; then
+  echo "<template> must be a directory containing a git repo"
+  usage
+  exit 2
+fi
+
+cat << EOF > /tmp/.gitignore
+node_modules
+*.swp
+EOF
+
+cp $cpArgs /tmp/.gitignore .
+
+cp $cpArgs $templateDir/.editorconfig .
+cp $cpArgs $templateDir/.eslintignore .
+cp $cpArgs $templateDir/.eslintrc.js .
+cp $cpArgs $templateDir/.prettierignore .
+cp $cpArgs $templateDir/.prettierrc .
+cp $cpArgs $templateDir/tsconfig.json .
