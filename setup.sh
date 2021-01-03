c17dc8ec63e6a996d3ff3f28aa90246219ca1524
Jon Newton <newt@hey.com>
2021-01-02 21:59:42 -0500

    add xfce and some notes on other software to install

--- setup.sh
+++ setup.sh
@@ -11,6 +11,21 @@ if [ $# -eq 1 ]; then
 
   mkdir -p $1/.config/
   ln -sn `pwd`/.config/nvim/ $1/.config/
+
+  ln -sn `pwd`/.config/xfce4/terminal/ $1/.config/xfce4/
+
+  echo "Software installs"
+  echo "  - sudo apt install git git-extras"
+  echo "  - sudo apt install neovim && nvim +PlugInstall +qall"
+  echo "  - sudo update-alternatives --config vim"
+  echo "  - sudo update-alternatives --config vi"
+  echo "  - vscode https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
+  echo "  - nvm https://github.com/nvm-sh/nvm"
+  echo "  - jekyll https://jekyllrb.com/docs/installation/ubuntu/"
+  echo "  - docker https://docs.docker.com/engine/install/ubuntu/"
 else
   echo "Usage: setup.sh <target dir>"
+  echo ""
+  echo "E.g."
+  echo "  setup.sh /home/foobar"
 fi
