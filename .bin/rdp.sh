d976ff1941e0b768a12a718fba95a346b4e75307
Jon Newton <newt@hey.com>
2021-01-04 17:09:11 -0500

    Add comment and update machine name

--- .bin/rdp.sh
+++ .bin/rdp.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-machine=columbia
+# store password with:
+# secret-tool store --label='RDP password' machine columbia
+#machine=columbia
+machine=jonesboro
 xfreerdp /d:atlas /u:jnewton /p:`secret-tool lookup machine $machine` /v:$machine.local /cert:ignore /f /smart-sizing
