9feef476360cd35f118da2a07350059c96c6380a
Jon Newton <newt@hey.com>
2021-01-10 22:05:26 -0500

    combine all into one

--- .bin/git-check.sh
+++ .bin/git-check.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
-#find all unpushed changes in all local repos on all branches
+# check state of each local repo
+# report on uncommitted changes and unpushed commits
 for d in `locate --regex "\/code\/[^\/]+\/\.git$"`
 do
   #remove '.git'
   d2=${d%.git}
   pushd $d2 > /dev/null
+
+  #find unpushed commits
   git log --branches --not --remotes --oneline --exit-code > /tmp/out
   if [ $? != 0 ]; then
     echo "${d2}  - unpushed change count: `cat /tmp/out | wc -l`"
   fi
+
+  #find uncommitted changes
+  lineCount=`git status -s | wc -l`
+  if [ $lineCount != 0 ]; then
+    echo "${d2}  - uncommitted change count: ${lineCount}"
+  fi
+
   popd > /dev/null
 done
--- .bin/unpushed.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# show log of unpushed changes in pwd
-git log --branches --not --remotes --decorate --oneline
