5ffda371159a8ebb0da958dae9794503bc2c3fa6
Jon Newton <newt@hey.com>
2024-10-01 06:34:23 -0400

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -34,6 +34,25 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
+  -- installed to fix https://github.com/nvim-lua/kickstart.nvim/pull/1040
+  {
+    'folke/lazydev.nvim',
+    ft = 'lua',
+    opts = {
+      library = {
+        -- Load luvit types when the `vim.uv` word is found
+        { path = 'luvit-meta/library', words = { 'vim%.uv' } },
+      },
+    },
+  },
+
+  {
+    'Bilal2453/luvit-meta',
+    lazy = true
+  },
+
+  -- end of fix for kickstart.nvim/1040
+
   {
     -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
@@ -47,9 +66,61 @@ require('lazy').setup({
       }
     },
   },
+
+  {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v3.x',
+    dependencies = {
+      'neovim/nvim-lspconfig',
+      'hrsh7th/cmp-nvim-lsp',
+      {
+        'hrsh7th/nvim-cmp',
+        dependencies =
+        { 'L3MON4D3/LuaSnip' }
+      },
+    }
+  },
+
 }, {})
 
+-- try out new default scheme
 vim.cmd.colorscheme "habamax"
+
+-------- Language server config
+local lsp_zero = require("lsp-zero")
+
+-- only enable keymaps when lsp is active for buffer
+lsp_zero.on_attach(function(_, bufnr)
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp_zero.default_keymaps({
+    buffer = bufnr,
+    -- overwrite existing mappings
+    preserve_mappings = false
+  })
+end)
+
+require('lspconfig').lua_ls.setup({})
+
+require('lspconfig').powershell_es.setup({
+  bundle_path = 'C:/tools/lsp/PowerShellEditorServices',
+  shell = 'powershell.exe'
+})
+
+-- deno LSP configuration
+-- To appropriately highlight codefences returned from denols, you will need to augment vim.g.markdown_fenced languages in your init.lua
+--vim.g.markdown_fenced_languages = {
+--  "ts=typescript"
+--}
+--require('lspconfig').denols.setup({})
+
+require('lspconfig').tsserver.setup({})
+
+require('lspconfig').rust_analyzer.setup({})
+
+require('lspconfig').gopls.setup({})
+
+require('lspconfig').gopls.setup({})
 
 -------- Key mappings
 
